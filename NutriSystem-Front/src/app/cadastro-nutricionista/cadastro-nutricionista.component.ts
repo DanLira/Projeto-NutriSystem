c580c41fcd0d539bcdbe49aa3e71ce2186398148
Rafael Joaquim <rafaelcybersistemas@gmail.com>
2019-10-17 20:11:30 -0300

    subindo a tela de cadastro de nutricionista funcionando

--- NutriSystem-Front/src/app/cadastro-nutricionista/cadastro-nutricionista.component.ts
+++ NutriSystem-Front/src/app/cadastro-nutricionista/cadastro-nutricionista.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Nutricionista } from './../model/nutricionista.model';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatTableDataSource, MatPaginator } from '@angular/material';
+import { NutricionistaService } from './nutricionista.service';
+import { ToastrService } from 'ngx-toastr';
 
 
 @Component({
@@ -8,10 +13,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroNutricionistaComponent implements OnInit {
 
-  
-  constructor() { }
+  nutricionista: Nutricionista[];
+
+  formsRegister: FormGroup;
+  filterFormNutricionista: FormGroup;
+  nutricionistaList: Nutricionista[];
+  displayedColumns: string[] = ['nome', 'email', 'crn', 'action'];
+  dataSource = new MatTableDataSource<Nutricionista>();
+  todoDataSource: any[];
+  @ViewChild('MatPaginator') MatPaginator: MatPaginator;
+
+
+
+  constructor(private readonly _formBuilder: FormBuilder,
+    private readonly _nutricionistaService: NutricionistaService, private readonly toastr: ToastrService) { }
 
   ngOnInit() {
+    this.dataSource.paginator = this.MatPaginator;
+    this.formsRegister = this._formBuilder.group({
+      id: [''],
+      nome: [''],
+      crn: [''],
+      sexo: [''],
+      email: ['']
+
+    });
+
+    this.filterFormNutricionista = this._formBuilder.group({
+      nomeFilterCtrl: [''],
+      emailFilterCtrl: [''],
+      cpfFilterCtrl: ['']
+    });
+
+  }
+  saveNutricionista() {
+    const nutricionista: Nutricionista = {
+      id: this.formsRegister.value.id,
+      nome: this.formsRegister.get('nome').value,
+      email: this.formsRegister.get('email').value,
+      sexo: this.formsRegister.get('sexo').value,
+      crn: this.formsRegister.get('crn').value
+    };
+
+
+    if (this.nutricionistaList.filter(x => x.email === this.formsRegister.get('email').value).length <= 0) {
+      this._nutricionistaService.saveNutricionista(nutricionista)
+        .subscribe(nutricionistaSave => {
+          //this.nutricionista = (!!nutricionistaSave) ? nutricionistaSave : [];
+          //this.dataSource.data = nutricionistaSave;
+          this.formsRegister.reset();
+        });
+      this.toastr.success('Nutriconista salvo com sucesso!', 'Salvar');
+    } else {
+      this.toastr.info('Este email já existe!', '');
+    }
+
+  }
+
+  clearNutricionista(): void {
+    this.dataSource.data = this.nutricionistaList;
+    this.formsRegister.value.id = null;
+    this.formsRegister.reset();
+    this.toastr.info('Campos limpos com sucesso!', 'Limpar');
   }
 
 }
--- NutriSystem-Front/src/app/cadastro-nutricionista/nutricionista.service.ts
+++ NutriSystem-Front/src/app/cadastro-nutricionista/nutricionista.service.ts
@@ -1,9 +1,59 @@
+import { async } from '@angular/core/testing';
+import { Nutricionista } from './../model/nutricionista.model';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import * as $ from 'jquery';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NutricionistaService {
 
-  constructor() { }
+   readonly apiUrl = 'https://localhost:44372'
+
+  constructor(private readonly _http: HttpClient) { }
+
+    getAllNutricionista() {
+        return this._http.get<Nutricionista[]>(this.apiUrl + '/Nutricionista/GetAll');
+    }
+    getNutricionistaById(idNutricionista: string): Observable<any> {
+        return this._http.get(this.apiUrl + '/Nutricionista/?id=' + idNutricionista);
+    }
+    saveNutricionista(nutricionista: Nutricionista) {
+        $.ajax({
+            type: "POST",
+            url: this.apiUrl + '/Nutricionista/Create',
+            content: "application/json; charset=utf-8",
+            dataType: "json",
+            data: nutricionista,
+            error: function (xhr, textStatus, errorThrown) {
+            }
+        });
+
+
+        return this._http.post(this.apiUrl + '/Nutricionista/Create', nutricionista);
+    }
+    editNutricionista(nutricionista: Nutricionista): Observable<any> {
+        return this._http.put(this.apiUrl + '/Nutricinonista/Update' + nutricionista.id, nutricionista);
+    }
+    deleteNutricionista(idNutricinoista: number): Observable<any> {
+        let responseDelete;
+        $.ajax({
+            type: "POST",
+            url: this.apiUrl + '/Nutricionista/Delete',
+            content: "application/json; charset=utf-8",
+            dataType: "json",
+            async: false,
+            data: {id : idNutricinoista},
+            success: function (response) {
+                responseDelete =  response;
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                responseDelete = textStatus;
+            }
+        });
+        return responseDelete;
+    }
+
 }
